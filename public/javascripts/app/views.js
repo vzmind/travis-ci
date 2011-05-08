8de0a90f2e1a9ffc370f84fdc0473634f5f321d5
Sven Fuchs <svenfuchs@artweb-design.de>
2011-05-09 01:02:02 +0200

    use {{else}} instead of setting emptyView

--- public/javascripts/app/views.js
+++ public/javascripts/app/views.js
@@ -3,22 +3,10 @@ Travis.Views = {
     tagName: 'ul',
     itemClass: 'job',
     contentBinding: 'Travis.controllers.jobs',
-    // emptyView: SC.View.extend({
-    //   render: function(context, firstTime) {
-    //     context = context.begin('li').push('No jobs');
-    //     sc_super();
-    //   }
-    // })
   }),
   Workers: SC.TemplateCollectionView.extend({
     tagName: 'ul',
     itemClass: 'worker',
     contentBinding: 'Travis.controllers.workers',
-    // emptyView: SC.View.extend({
-    //   render: function(context, firstTime) {
-    //     context = context.begin('li').push('No workers');
-    //     sc_super();
-    //   }
-    // })
   }),
 };
