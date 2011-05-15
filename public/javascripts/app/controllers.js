21f00bbbb3d0122e641c9dfe12fb1b38fe02b9aa
tomhuda <tomhuda@strobecorp.com>
2011-05-15 00:40:24 -0700

    Update controller layer to lean more heavily on bindings instead of a series of load() calls.

--- public/javascripts/app/controllers.js
+++ public/javascripts/app/controllers.js
@@ -1,37 +1,37 @@
 Travis.controllers = SC.Object.create({
+
   repositories: SC.ArrayController.create({
     load: function() {
       this.set('content', Travis.Repository.latest());
-    },
+    }
   }),
+
   repository: SC.SingleObjectController.create({
     tabs: SC.Object.create({
       active: null,
       current: SC.ObjectController.create(Travis.Helpers.Build, {
         name: 'current',
-        load: function(repository, params) {
-          this.set('content', repository.lastBuild());
-        }
+        contentBinding: 'Travis.controllers.repository.lastBuild'
       }),
+
       builds: SC.ArrayController.create(Travis.Helpers.Build, {
         name: 'builds',
-        load: function(repository, params) {
-          this.set('content', repository.builds());
-        },
+        contentBinding: 'Travis.controllers.repository.builds'
       }),
+
       build: SC.ObjectController.create(Travis.Helpers.Build, {
         name: 'build',
-        load: function(repository, params) {
-          this.set('content', Travis.Build.find(params.id));
-        },
+        content: null
       })
     }),
+
     load: function(params) {
       this.params = params;
-      this.activateTab(params.tab);
-      this.set('content', null);
+      this.setPath('tabs.build.content', Travis.Build.find(this.get('buildId')));
       this.set('content', this.repository(params));
+      this.activateTab(params.tab);
     },
+
     repository: function(params) {
       if(params.owner && params.repository) {
         return Travis.Repository.bySlug(params.owner + '/' + params.repository);
@@ -39,30 +39,30 @@ Travis.controllers = SC.Object.create({
         return Travis.Repository.latest();
       }
     },
+
     activateTab: function(tab) {
-      this.tabs.set('active', this.tabs[tab]);
-      $('#repository .tabs > li').removeClass('active');
-      $('#repository #tab_' + tab).addClass('active');
+      this.invokeLast(function() {
+        this.tabs.set('active', this.tabs[tab]);
+        $('#repository .tabs > li').removeClass('active');
+        $('#repository #tab_' + tab).addClass('active');
+      });
     },
+
     repositoryObserver: function() {
       var repository = this.getPath('content.firstObject');
-      if(repository) {
-        var activeTab = this.tabs.get('active');
-        activeTab.set('content', null);
-        activeTab.load(repository, this.params);
-        repository.select();
-      }
-    }.observes('*content'),
+      if(repository) { repository.select(); }
+    }.observes('content', 'tabs.active')
   }),
+
   jobs: SC.ArrayController.create({
     load: function() {
       this.set('content', Travis.Job.all());
     }
   }),
+
   workers: SC.ArrayController.create({
     load: function() {
       this.set('content', Travis.Worker.all());
     }
-  }),
+  })
 });
-
